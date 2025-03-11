1cb2ff4886b338ad48d31911ea4eed05b35a4ba9
Michael Lejno <mblejano07@gmail.com>
2025-03-11 14:28:30 +0800

    with restriction

--- addons/event_membership_update/models/res_partner.py
+++ addons/event_membership_update/models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -6,7 +6,13 @@ class ResPartner(models.Model):
     purchased_membership_id = fields.Many2one(
         'product.product',
         string='Purchased Membership',
-        domain=[('membership', '=', True)],
-        related='member_lines.membership_id',
+        compute='_compute_purchased_membership',
         store=True
     )
+
+    @api.depends('member_lines.membership_id')
+    def _compute_purchased_membership(self):
+        for partner in self:
+            # Get the latest membership_id (or any logic you want)
+            membership = partner.member_lines.filtered(lambda m: m.membership_id).sorted('id', reverse=True)[:1]
+            partner.purchased_membership_id = membership.membership_id if membership else False
