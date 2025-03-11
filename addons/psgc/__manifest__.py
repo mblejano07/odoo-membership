1867e7a4465d46086baacd063318fd6613228b44
Michael Lejno <mblejano07@gmail.com>
2025-03-11 16:59:34 +0800

    barangay without import

--- addons/psgc/__manifest__.py
+++ addons/psgc/__manifest__.py
@@ -13,11 +13,14 @@
         'data/region.xml',
         'data/province.xml',
         'data/city.xml',
+        'data/barangay.xml',
         'views/region_form.xml',
         'views/province_form.xml',
+        'views/barangay_form.xml',
         'views/city_form.xml',   # ✅ Load city form first
         'views/psgc_menu.xml',
         'views/city_action.xml', # ✅ Load city action after
+        'views/barangay_action.xml',
         'views/region_action.xml',
         'views/province_action.xml',
         'security/ir.model.access.csv',
