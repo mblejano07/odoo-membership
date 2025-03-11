39a322ae0d46110b5a848d9b30bddfefb77c9f7a
Michael Lejno <mblejano07@gmail.com>
2025-03-11 16:37:14 +0800

    fix bug

--- addons/psgc/__manifest__.py
+++ addons/psgc/__manifest__.py
@@ -13,13 +13,13 @@
         'data/region.xml',
         'data/province.xml',
         'data/city.xml',
-        'views/psgc_menu.xml',
-        'views/region_action.xml',
-        'views/province_action.xml',
-        'views/city_action.xml',
         'views/region_form.xml',
         'views/province_form.xml',
-        'views/city_form.xml',
+        'views/city_form.xml',   # ✅ Load city form first
+        'views/psgc_menu.xml',
+        'views/city_action.xml', # ✅ Load city action after
+        'views/region_action.xml',
+        'views/province_action.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,
